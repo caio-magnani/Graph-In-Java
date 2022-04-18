5aaf11595c555b026c1a074b5a3579b61e959f21
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-04-18 00:05:59 -0300

    Update Matrix.java

--- src/graph/matrix/Matrix.java
+++ src/graph/matrix/Matrix.java
@@ -1,7 +1,9 @@
 package graph.matrix;
 
-public class Matrix{
-    int[][] matrix;
+import java.util.ArrayList;
+
+public class Matrix {
+    private int[][] matrix;
     private int c;
     private int l;
     /*
@@ -13,44 +15,68 @@ public class Matrix{
 
     public Matrix() {
         this.matrix = new int[1][1];
-        this.c = 1;
+        this.l = 1;
         this.c = 1;
     }
 
-    public int get(int l, int c){
+    public int getL() {
+        return l;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public int get(int l, int c) {
         return this.matrix[l][c];
     }
 
+    public ArrayList<Integer> getColumn(int c) {
+        ArrayList<Integer> column = new ArrayList<Integer>();
+        for (int l = 0; l < this.l; l++) {
+            column.add(this.matrix[l][c]);
+        }
+        return column;
+    }
+
     public void removeColumn(int column) {
-        int[][] aux = new int[this.l][this.c-1];
+        int[][] aux = new int[this.l][this.c - 1];
         for (int l = 0; l < this.l; l++) {
             for (int c = 0; c < this.c; c++) {
-                if(c < column){
+                if (c < column) {
                     aux[l][c] = this.matrix[l][c];
                 }
-                if(c > column)
-                    aux[l][c-1]=this.matrix[l][c];
+                if (c > column)
+                    aux[l][c - 1] = this.matrix[l][c];
             }
         }
         this.matrix = aux;
-        this.c-=1;
+        this.c -= 1;
+    }
+
+    public ArrayList<Integer> getLine(int l) {
+        ArrayList<Integer> line = new ArrayList<Integer>();
+        for (int c = 0; c < this.c; c++) {
+            line.add(this.matrix[l][c]);
+        }
+        return line;
     }
 
     public void removeLine(int line) {
-        int[][] aux = new int[this.l-1][this.c];
+        int[][] aux = new int[this.l - 1][this.c];
         for (int c = 0; c < this.c; c++) {
             for (int l = 0; l < this.l; l++) {
-                if(l == line)
+                if (l == line)
                     aux[l][c] = 0;
-                else if(l<line){
+                else if (l < line) {
                     aux[l][c] = this.matrix[l][c];
                 }
-                if(l>line)
-                    aux[l-1][c]=this.matrix[l][c];
+                if (l > line)
+                    aux[l - 1][c] = this.matrix[l][c];
             }
         }
         this.matrix = aux;
-        this.l-=1;
+        this.l -= 1;
     }
 
     // Matrix Manupulation
@@ -59,12 +85,12 @@ public class Matrix{
         try {
             this.matrix[line][column] = value;
         } catch (ArrayIndexOutOfBoundsException e) {
-            if(line > this.l - 1)
-                this.l=line+1;
-            if(column > this.c - 1)
-                this.c=column+1;
+            if (line > this.l - 1)
+                this.l = line + 1;
+            if (column > this.c - 1)
+                this.c = column + 1;
             grow();
-            this.matrix[line][column]=value;
+            this.matrix[line][column] = value;
         }
     }
 
@@ -85,8 +111,8 @@ public class Matrix{
     @Override
     public String toString() {
         String s = new String();
-        for(int l = 0; l < this.l; l++){
-            for(int c = 0; c<this.c ; c++){
+        for (int l = 0; l < this.l; l++) {
+            for (int c = 0; c < this.c; c++) {
                 s += "|" + this.matrix[l][c];
             }
             s += "|\n";
