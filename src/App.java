9e8f5c1c336b6b118820628d9fec869aa5eb5487
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-04-18 00:09:38 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,23 +1,22 @@
-import graph.matrix.Matrix;
+import graph.matrix.Adjacency;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Matrix m = new Matrix();
-        m.set(0, 0, 1);
+    public static void main(String[] args) {
+        Adjacency m = new Adjacency();
+        m.addVertex(1);
+        m.addVertex(2);
+        m.addVertex(3);
+        m.addVertex(4);
+        m.addVertex(5);
+        m.addVertex(6);
+        m.addEgdeUndirectional(1, 2);
+        m.addEgdeDirectional(4, 5);
+        m.addLoop(1);
+        System.out.println("\nMatriz de Adjacencia:\n");
         System.out.println(m);
-        m.set(1, 1, 2);
-        System.out.println(m);
-        m.set(2, 2, 3);
-        System.out.println(m);
-        m.set(3, 3, 4);
-        System.out.println(m);
-        m.set(4, 4, 5);
-        System.out.println(m);
-        m.set(5, 5, 6);
-        System.out.println(m);
-        m.removeLine(1);
-        System.out.println(m);
-        m.removeColumn(5);
-        System.out.println(m);
+        System.out.println("Grau + do Vertice " + 1 + " = " + m.getDegreePositiveOfVertex(1));
+        System.out.println("Grau + do Vertice " + 4 + " = " + m.getDegreePositiveOfVertex(4));
+        System.out.println("Grau + do Vertice " + 5 + " = " + m.getDegreePositiveOfVertex(5));
+        System.out.println("Grau - do Vertice " + 5 + " = " + m.getDegreeNegativeOfVertex(5));
     }
 }
