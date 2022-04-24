7531b927b2d67c9512eb64f824d40898319ae363
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-04-24 20:39:37 -0300

    Update

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import map.Map;
 import services.CsvReader;
+import services.DecoStrings;
 
 public class App {
     public static void main(String[] args) {
@@ -12,7 +13,8 @@ public class App {
     }
 
     public static void loadFileToMap(Map map) {
-        File br = new File("../database/br.csv");
+        File br = new File("../src/database/br.csv");
+        System.out.println(DecoStrings.RED("path = " + br.getAbsolutePath()));
         System.out.println(br.exists());
         ArrayList<String> linesOfFile = new ArrayList<>();
         try {
